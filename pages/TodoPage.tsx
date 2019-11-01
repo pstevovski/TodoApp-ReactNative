eeb08351943727d504921d2f4d78ee9c5000a11b
pecko95 <stevovski.petar@gmail.com>
2019-11-01 16:09:55 +0100

    Added option to select and update specific item from list

--- pages/TodoPage.tsx
+++ pages/TodoPage.tsx
@@ -12,6 +12,8 @@ const TodoPage = () => {
   const [todoText, setTodoText] = useState("")
   const { getItem, setItem } = useAsyncStorage("@todoList");
   const [itemArray, setItemArray] = useState([]);
+  const [editState, setEditState] = useState(false);
+  const [oldTodoText, setOldTodoText] = useState("");
 
   // Read items from storage on page load
   useEffect(() => {
@@ -28,8 +30,6 @@ const TodoPage = () => {
     } else {
       setItemArray([]);
     }
-
-    console.log(`Item: ${item}`);
   }
 
   // Add items to the array and save to storage
@@ -53,7 +53,45 @@ const TodoPage = () => {
 
     // Update list
     readItemFromStorage();
+
+    setTodoText("");
+    setEditState(false);
   }
+
+  // Select item and update text in input field to match the item text
+  const selectItem = async (id: number) => {
+    let item = itemArray[id];
+    
+    setOldTodoText(item);
+    setTodoText(item);
+
+    // Set edit mode
+    setEditState(true);
+  }
+
+  const updateItem = async () => {
+    const findItem = itemArray.find(item => item === oldTodoText);
+    const itemId   = itemArray.indexOf(findItem!);
+
+    // Make a copy of the array and edit specific item
+    let newItemValue: string = todoText;
+    const updatedArray = [...itemArray];
+    updatedArray.splice(itemId, 1, newItemValue);
+
+    // Update the current array
+    setItemArray(updatedArray);
+
+    // Save the updated array to storage
+    await setItem(JSON.stringify(updatedArray))
+
+    // Update list
+    readItemFromStorage();
+
+    // Clear input and reset state
+    setEditState(false);
+    setTodoText("");
+  }
+
 
   return (
     <View>
@@ -69,11 +107,15 @@ const TodoPage = () => {
 
       {itemArray && itemArray.length > 0 ?
         itemArray.map((item, index) => (
-          <Text key={index}>{item}</Text>
+          <Text key={index} onPress={() => selectItem(index)}>{item}</Text>
         ))
       : null}
       
-      <Text onPress={handleAddItem} style={text.a}>ADD TODO</Text>
+        {editState ? 
+          <Text onPress={updateItem} style={text.a}>Edit Item</Text> 
+        :
+          <Text onPress={handleAddItem} style={text.a}>ADD TODO</Text>
+        }
 
       <Text onPress={clearStorage} style={text.p}>CLEAR STORAGE</Text>
     </View>
