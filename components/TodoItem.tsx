dc74fc8760c86085d72223b0756e932193d8afc0
pecko95 <stevovski.petar@gmail.com>
2019-11-06 17:11:00 +0100

    Added option to go to edit todo page when clicked on a todo

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { withNavigation } from "react-navigation";
 
 interface TodoItemProps {
   todo: string,
   todoID: string,
   completed: boolean,
   date?: Date | string,
-  onPress: (todoID: string) => void
+  onPress: (todoID: string) => void,
+  navigation: any
 }
 
 const TodoItem = (props: TodoItemProps) => {
   return (
-    <TouchableOpacity onLongPress={() => props.onPress(props.todoID)}>
+    <TouchableOpacity 
+      onLongPress={() => props.onPress(props.todoID)}
+      onPress={() => props.navigation.navigate("CreateTodo", {
+        type: "item",
+        title: props.todo,
+        listTitleInput: props.todo,
+        state: "edit"
+      })}
+    >
       <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
         <View>
           <Text style={{
@@ -36,4 +46,4 @@ const TodoItem = (props: TodoItemProps) => {
   )
 }
 
-export default TodoItem;
+export default withNavigation(TodoItem);
