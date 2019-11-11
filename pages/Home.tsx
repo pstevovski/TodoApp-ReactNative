b23a06da49a572d66a85a033cda7cce0ddb81d8b
pecko95 <stevovski.petar@gmail.com>
2019-11-11 10:33:31 +0100

    Updated screen structure, removed unused code.

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useAsyncStorage } from "@react-native-community/async-storage";
 
 // React native elements
 import { View, Text, TouchableOpacity, Dimensions } from "react-native";
@@ -11,45 +12,41 @@ import { text, containers, buttons } from "../styles/styles";
 import MainList from "../components/MainList";
 
 const Home = (props: any) => {
+  // Fetch data from local storage to check if there are todos saved
+  const [lists, setLists] = useState([]);
+  const { getItem } = useAsyncStorage("@todoList");
+
+  useEffect(() => {
+    readFromStorage();
+  })
+
+  const readFromStorage = async () => {
+    const savedLists = await getItem();
+
+    if (savedLists !== null) {
+      setLists(JSON.parse(savedLists));
+    } else {
+      setLists([]);
+    }
+
+  }
+
   return (
     <View>
-      <MainList />
-
-      <Icon
-        onPress={() => props.navigation.navigate("CreateTodo", { 
-          type: "list",
-          title: "Create New List"
-        })}
-        name="add-circle" 
-        size={70} 
-        color="#e1302a" style={{
-          position: "absolute",
-          right: 20,
-          top: Dimensions.get("screen").height - 170,
-        }}
-      />
-      {/* <View style={containers.homePageContainer}>
-        <TouchableOpacity onPress={() => props.navigation.navigate("Todos", {
-          itemID: Math.floor(Math.random() * 50)
-        })}>
-          <View style={[buttons.global, buttons.md]}>
-            <Text style={text.pButtonMd}>Go to list</Text>
-          </View>
-        </TouchableOpacity>
-      </View> */}
-
-      
-      {/* <TouchableOpacity onPress={() => props.navigation.navigate("Todo")}>
-        <View style={[buttons.global, buttons.sm, colors.red]}>
-          <Text style={{color: "#fff"}}>Todo list</Text>
+      {lists && lists.length > 0 ?
+        <MainList />
+      :
+        <View>
+          <Text>No lists found.</Text>
+          <Text>Create a new one?</Text>
+          <TouchableOpacity onPress={() => props.navigation.navigate("CreateTodo", {
+            type: "list",
+            title: "Create New List"
+          })}>
+            <Text>New</Text>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => props.navigation.navigate("Animation")}>
-        <View style={[buttons.global, buttons.sm, colors.red]}>
-          <Text style={{color: "#fff"}}>Animations</Text>
-        </View>
-      </TouchableOpacity> */}
-
+      }
     </View>
   )
 }
